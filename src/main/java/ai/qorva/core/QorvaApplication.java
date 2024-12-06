f2d7e4cfabcb1d780af2faff75e8813edb95f8de
Mohamed Toure <demahom.toure@gmail.com>
2024-12-06 16:26:35 +0100

    initial setup

---
+++ src/main/java/ai/qorva/core/QorvaApplication.java
@@ -0,0 +1,13 @@
+package ai.qorva.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QorvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QorvaApplication.class, args);
+	}
+
+}
